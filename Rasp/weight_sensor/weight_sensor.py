d74ded300b9f40ecca883b4940eb67ed9cb04b18
CarlosPena00 <chcp@cin.ufpe.br>
2017-07-24 02:45:43 -0300

    Change the DT call, now the coding is working

--- Rasp/weight_sensor/weight_sensor.py
+++ Rasp/weight_sensor/weight_sensor.py
@@ -5,8 +5,6 @@ import time
 class WeightSensor:
     """ Class that get the weight from a HX711
     this module is based on the HX711 datasheet
-
-    Not test yet
     """
 
     def __init__(self, SCK, DT):
@@ -17,20 +15,16 @@ class WeightSensor:
         GPIO.setup(self.DT, GPIO.IN)  # Device Output
 
     def getWeight(self):
+        while GPIO.input(self.DT) == 1:
+            pass
         weight = 0
-        GPIO.output(self.SCK, False)
-        while GPIO.input(self.DT):
-            for i in range(0, 24):
-                GPIO.output(self.SCK, True)
-                time.sleep(0.01)
-                weight = weight << 1
-                GPIO.output(self.SCK, False)
-                if GPIO.input(self.DT):
-                    weight += 1
-                if weight > 100000000:
-                    return 0
+        for i in range(0, 24):
+            weight = weight << 1
+            GPIO.output(self.SCK, True)
+            if GPIO.input(self.DT):
+                weight += 1
+            GPIO.output(self.SCK, False)
         GPIO.output(self.SCK, True)
-        weight ^= 0x800000
         GPIO.output(self.SCK, False)
         return weight
 
@@ -42,9 +36,9 @@ if __name__ == "__main__":
     try:
         while True:
             print (WS.getWeight())
-            time.sleep(1)
+            time.sleep(0.5)
     except KeyboardInterrupt:
         GPIO.cleanup()
-        print ("lean GPIO")
+        print ("lean GPIO")  # Clean the GPIO
 
 
