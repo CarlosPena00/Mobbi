03c2a9bddb805282eaa09f3dea3876515feda427
CarlosPena00 <chcp@cin.ufpe.br>
2017-07-24 01:12:25 -0300

    Adding time between call's and cleaning GPIO

--- Rasp/weight_sensor/weight_sensor.py
+++ Rasp/weight_sensor/weight_sensor.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 
 class WeightSensor:
@@ -11,7 +12,7 @@ class WeightSensor:
     def __init__(self, SCK, DT):
         self.SCK = SCK
         self.DT = DT
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.SCK, GPIO.OUT)  # SCK command
         GPIO.setup(self.DT, GPIO.IN)  # Device Output
 
@@ -21,11 +22,29 @@ class WeightSensor:
         while GPIO.input(self.DT):
             for i in range(0, 24):
                 GPIO.output(self.SCK, True)
+                time.sleep(0.01)
                 weight = weight << 1
                 GPIO.output(self.SCK, False)
                 if GPIO.input(self.DT):
                     weight += 1
+                if weight > 100000000:
+                    return 0
         GPIO.output(self.SCK, True)
         weight ^= 0x800000
         GPIO.output(self.SCK, False)
         return weight
+
+
+if __name__ == "__main__":
+    SCK_PIN = 26
+    DT_PIN = 20
+    WS = WeightSensor(SCK_PIN, DT_PIN)
+    try:
+        while True:
+            print (WS.getWeight())
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print ("lean GPIO")
+
+
