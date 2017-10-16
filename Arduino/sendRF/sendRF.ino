0391a37e47f30096169ce97a4354ba2c2dbf3257
chcp <chcp@cin.ufpe.br>
2017-10-15 23:26:43 -0300

    Adding V0.1 of recive code

--- Arduino/sendRF/sendRF.ino
+++ Arduino/sendRF/sendRF.ino
@@ -1,24 +1,57 @@
 #include <SPI.h>
 #include <RF24.h>
+#include "printf.h"
 
 // ce, csn pins
-RF24 radio(9,10);
+RF24 radio(9, 10);
+
+byte ID[4];
+char rxBuff[14];
+int state = 3;
+
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(0x76);
-  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.openWritingPipe(0xE8E8F0F0E2LL);
+  radio.openReadingPipe(1, 0xE8E8F0F0E1LL);
   radio.enableDynamicPayloads();
+  radio.setAutoAck(true);
   radio.powerUp();
+  radio.printDetails();
   
+  radio.startListening();
+
+}
+
+void ForceSendID()
+{
+
+  if (radio.available())
+  {
+    radio.read(rxBuff, 14);
+    for (int i = 0 ; i < 14 ; i ++)
+      Serial.print(char(rxBuff[i]));
+    Serial.println(" ");
+    if (rxBuff[0] == '*')
+    {
+      Serial.print("Send Back: ");
+      Serial.println(rxBuff[1]);
+      radio.stopListening();
+      radio.write(&rxBuff[1], 1);  
+      radio.startListening();
+    }
+    else{
+      Serial.println("Verify Byte Error");
+    }
+
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  const char text[] = "Hello World!";
-  radio.write(&text, sizeof(text));
-  delay(1000);
-  
+  ForceSendID();
+  delay(100);
 }
